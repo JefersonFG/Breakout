25460b53b46fbe1e39a0642a5d178f8d12d3ea93
Jéferson Ferreira Guimarães <jefersonfguimaraes@gmail.com>
2015-10-20 11:04:34 -0200

    Commit inicial do projeto Breakout

---
+++ display.h
@@ -0,0 +1,9 @@
+//Arquivo com as funções que exibem informações na tela, como o fundo do jogo e dos menus
+
+#include <stdio.h>
+
+//Função que exibe o menu principal
+void showMenu()
+{
+    printf("\nMenu!\n");
+}
---
+++ main.c
@@ -0,0 +1,12 @@
+//Arquivo principal de código do projeto Breakout
+
+#include <stdio.h>
+#include <display.h>
+
+int main(void)
+{
+    printf("Hello World!\n");
+    showMenu();
+    return 0;
+}
+
