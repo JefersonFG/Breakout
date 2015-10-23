6a1ade0d64f21e6bdacaf9b4daa7ab5e91d2a801
Jéferson Ferreira Guimarães <jefersonfguimaraes@gmail.com>
2015-10-23 14:46:22 -0200

    Adicionada documentação à algumas funções

--- main.c
+++ main.c
@@ -6,13 +6,18 @@
 
 int main(void)
 {
+    //Seta o tamanho de tela para 40x80
+    //A linha extra é temporária enquanto o cursor permanece na última linha
+    //No futuro o cursor será realocado para o menu
     system("mode 80, 41");
 
     //Exibe o menu
     showMenu();
 
+    //É utilizado o getchar para o programa não finalizar após printar o conteúdo na tela
     getchar();
 
+    //Finaliza o programa
     return 0;
 }
 
